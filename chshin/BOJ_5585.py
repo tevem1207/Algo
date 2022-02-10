8566a017d4f02f6c0c305bb3471f78f498e6914d
chshin59 <chshin59@gmail.com>
2022-02-10 23:00:19 +0900

    [BOJ] 거스름돈 / 브론즈2 / 10분
    https://www.acmicpc.net/problem/5585

---
+++ chshin/BOJ_5585.py
@@ -0,0 +1,11 @@
+M = 1000 - 1
+
+changes = [500, 100, 50, 10, 5, 1]
+answer = 0
+for change in changes:
+    if M // change > 0:
+        answer += M // change
+        M %= change
+    if M == 0:
+        break
+print(answer)
