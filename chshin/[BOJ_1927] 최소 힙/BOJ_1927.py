d3229d33edaf7c72f15e92823257862ea50a2bd4
chshin59 <chshin59@gmail.com>
2022-03-10 21:03:09 +0900

    220310

---
+++ chshin/[BOJ_1927] 최소 힙/BOJ_1927.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+numbers = []
+for _ in range(N):
+    x = int(input().rstrip())
+    if x == 0:
+        if numbers:
+            print(heapq.heappop(numbers))
+        else:
+            print(0)
+    else:
+        heapq.heappush(numbers, x)
