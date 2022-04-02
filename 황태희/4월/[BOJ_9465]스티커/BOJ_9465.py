b656f70e2fe8c438cb84d61cbde53819f68e22ac
황태희 <tevem1207@gmail.com>
2022-04-02 18:50:42 +0900

    0402 BOJ_9465

---
+++ 황태희/4월/[BOJ_9465]스티커/BOJ_9465.py
@@ -0,0 +1,16 @@
+import sys 
+sys.stdin = open('input1.txt')
+
+T = int(input())
+
+for tc in range(T):
+    matrix = [[], []]
+    N = int(input())
+    for i in range(2):
+        matrix[i] = [0] + list(map(int, input().split()))
+    dp = [[0 for _ in range(N+1)] for _ in range(2)]
+    for i in range(1, N+1):
+        dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + matrix[0][i]
+        dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + matrix[1][i]
+
+    print(max(dp[0][-1], dp[1][-1]))
