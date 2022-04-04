ea67fbca84976e617da7acdbba8431e817eab1fb
황태희 <tevem1207@gmail.com>
2022-04-04 11:49:54 +0900

    0404 BOJ_2527

--- 황태희/2월/[BOJ_2527]직사각형/BOJ_2527.py
+++ 황태희/2월/[BOJ_2527]직사각형/BOJ_2527.py
@@ -1,7 +1,10 @@
+import sys
+sys.stdin = open('input.txt')
+
 for _ in range(4):
     x1, y1, p1, q1, x2, y2, p2, q2 = map(int, input().split())
 
-    if (x1, y1) == (p2, q2) or (p1, y1) == (x2, q2) or (p1, q1) == (x2, y2) or (x1, q1) == (q2, y2):
+    if (x1, y1) == (p2, q2) or (p1, y1) == (x2, q2) or (p1, q1) == (x2, y2) or (x1, q1) == (p2, y2):
         print('c')
 
     elif p1 < x2 or p2 < x1 or q1 < y2 or q2 < y1:
