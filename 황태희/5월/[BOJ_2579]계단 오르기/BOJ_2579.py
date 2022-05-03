bb831a0aa4eabf3883cc16dd673021f882ebceda
황태희 <tevem1207@gmail.com>
2022-05-03 20:12:25 +0900

    0503 BOJ_2579

--- 황태희/5월/[BOJ_2579]계단 오르기/BOJ_2579.py
+++ 황태희/5월/[BOJ_2579]계단 오르기/BOJ_2579.py
@@ -12,6 +12,6 @@ dp[1] = stairs[0] + stairs[1]
 dp[2] = max(stairs[1] + stairs[2], stairs[0] + stairs[2])
 
 for i in range(3, n):
-    dp[i] = max(dp[i-3] + stairs[i] + stairs[i-1], dp[i-2] + stairs[i])
+    dp[i] = max(dp[i-3] + stairs[i-1], dp[i-2]) + stairs[i]
 
 print(dp[n-1])
