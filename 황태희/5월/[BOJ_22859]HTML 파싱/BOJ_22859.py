388c46338d472a59018c8a70c0c9feb85664fde1
황태희 <tevem1207@gmail.com>
2022-05-08 18:35:49 +0900

    0508 BOJ_20859

---
+++ 황태희/5월/[BOJ_22859]HTML 파싱/BOJ_22859.py
@@ -0,0 +1,27 @@
+import sys 
+sys.stdin = open('input1.txt') 
+
+html = input()
+tmp = ''
+i = 0
+
+while i < len(html):
+    if html[i] == '<':
+        while html[i] != '>':
+            if html[i] == '"':
+                i += 1
+                tmp = 'title : '
+                while html[i] != '"':
+                    tmp += html[i]
+                    i += 1
+                tmp = tmp.strip()
+                print(tmp)
+                tmp = ''
+            i += 1
+    if html[i] != '>':
+        tmp += html[i]
+    elif html[i-3:i+1] == '</p>':
+        tmp = tmp.strip().split()
+        print(' '. join(tmp))
+        tmp = ''
+    i += 1
